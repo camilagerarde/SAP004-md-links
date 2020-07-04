0449bb7f0d8531afe83db27743c186a85922cd90
Camila Cunha <millagerarde@gmail.com>
2020-07-04 19:49:29 -0300

    lendo o arquivo

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-module.exports = () => {
-  // ...
-};
+// module.exports = () => {
+//   // ...
+// };
+
+const fs = require('fs');
+fs.readFile('./README.md', 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
+console.log('depois');
