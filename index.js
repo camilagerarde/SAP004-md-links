4743789d33c4107abf884befcfcc9bb65277d20e
Camila Cunha <millagerarde@gmail.com>
2020-07-04 20:03:33 -0300

    teste

--- index.js
+++ index.js
@@ -3,11 +3,27 @@
 // };
 
 const fs = require('fs');
-fs.readFile('./README.md', 'utf8', (err, data) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
+
+// fs.readFile('./README.md', 'utf8', (err, data) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(data);
+//   }
+// });
+
+fs.readdir('./test', (err, files) => {
+  if (err) console.log(err);
+  else {
+    console.log('Current directory filenames:');
+    files.forEach((file) => {
+      console.log(file);
+    });
   }
 });
-console.log('depois');
+
+// const regExp = /\(\d{2,3}\)\s\d{4,5}-?\d{4}/g;
+// let telefone =
+//   'o telefone é (48) 12345-6789 tratar com João, o telefone é (48) 00000-6789 tratar com Maria';
+
+// console.log(telefone.match(regExp));
