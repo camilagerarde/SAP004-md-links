e809e68943a245658da0001ae272afa89a2f0bc0
Camila Cunha <millagerarde@gmail.com>
2020-07-10 15:10:59 -0300

    stats

--- cli.js
+++ cli.js
@@ -6,28 +6,35 @@ const package = require('./package.json');
 const mdLinks = require('./index');
 
 program.version(package.version);
-program.option('-v, --validate [validate]').parse(process.argv);
+program
+  .option('-v, --validate [validate]')
+  .option('-s, --stats [stats]')
+  .parse(process.argv);
 
 let path = program.args[0];
-let option = program.validate;
+let option = program.validate | program.stats;
 
 mdLinks(path, option)
   .then((response) => {
-    response.forEach((item) => {
-      if (option) {
+    if (option === 1 && program.validate) {
+      response.forEach((item) => {
         console.log(
           `\nFile: ${chalk.bold(item.file)} \nURL: ${chalk.magenta(
             item.href
           )} \nText: ${item.text} \nStatus: ${chalk.cyan(item.validate)}`
         );
-      } else {
+      });
+    } else if (option === 1 && program.stats) {
+      console.log(`Total de links encontrados: ${response.length}`);
+    } else {
+      response.forEach((item) => {
         console.log(
           `\nFile: ${chalk.bold(item.file)} \nURL: ${chalk.magenta(
             item.href
           )} \nText: ${item.text}`
         );
-      }
-    });
+      });
+    }
   })
   .catch((err) => {
     console.log(err);
